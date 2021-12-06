ccbc33df143f227a751d25dff537cfd2ab7e9e33
gauravc1617 <gauravc1617@gmail.com>
2021-12-06 13:02:51 +0100

    Adding new method

--- handler/product.go
+++ handler/product.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"../data"
 )
@@ -20,6 +22,24 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, rd *http.Request) {
 		p.getProducts(rw, rd)
 		return
 	}
+
+	if rd.Method == http.MethodPost {
+		p.addProduct(rw, rd)
+	}
+
+	if rd.Method == http.MethodPut {
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(rd.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "INvalid Uri", http.StatusBadRequest)
+			return
+		}
+		idString := g[0][1]
+		id, _ := strconv.Atoi(idString)
+		p.updateProduct(id, rw, rd)
+		p.l.Print(id)
+	}
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
@@ -29,5 +49,31 @@ func (p *Products) getProducts(rw http.ResponseWriter, rd *http.Request) {
 	err := lp.ToJson(rw)
 	if err != nil {
 		http.Error(rw, "Cannot marshall product", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, rd *http.Request) {
+	prod := &data.Product{}
+	err := prod.FromJson(rd.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshall to json", http.StatusBadRequest)
+	}
+	data.AddProduct(prod)
+	p.l.Printf("Prod %#v", prod)
+}
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, rd *http.Request) {
+	prod := &data.Product{}
+	err := prod.FromJson(rd.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshall to json", http.StatusBadRequest)
+	}
+	error1 := data.UpdateProduct(id, prod)
+
+	if error1 == data.ErrorProductNotFOund {
+		http.Error(rw, "Product nmot found", http.StatusNotFound)
+	}
+
+	if err != nil {
+		http.Error(rw, "Product nmot found", http.StatusInternalServerError)
 	}
 }
