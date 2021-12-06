f6fa2797f7029796595013c27057a85f7f910c8f
gauravc1617 <gauravc1617@gmail.com>
2021-12-06 15:08:24 +0100

    Adding gorilla framwork

--- httpapi.go
+++ httpapi.go
@@ -9,18 +9,28 @@ import (
 	"time"
 
 	"./handler"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	lp := handler.NewProduct(l)
+	ph := handler.NewProduct(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", lp)
+	sm := mux.NewRouter()
+	getRouter := sm.Methods("GET").Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProduct)
+	putRouter.Use(ph.MiddlewareProductValidation)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareProductValidation)
 
 	ser := &http.Server{
-		Addr:         ":9090",
+		Addr:         ":9091",
 		Handler:      sm,
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
